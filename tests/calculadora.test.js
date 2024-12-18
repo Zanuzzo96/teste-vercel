b9f021bf609c56023501833e082f86dde08074dc
Zanuzzo96 <felipezanuzzo@outlook.com>
2024-12-18 16:51:36 +0000

    criado o primeiro teste para exemplo

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,6 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
 });
